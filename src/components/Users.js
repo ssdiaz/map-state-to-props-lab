4dc6fe5e05e7c1572f3a3fc4eaa0172ea974409d
Samantha Diaz <samidiaz3@gmail.com>
2022-03-15 12:00:23 -0400

    commit tree 4a995c3904023a04f0cc652a95e128601a8b0bc8

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 // add any needed imports here
+import { connect } from 'react-redux';
+
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map((user, index) => 
+      <li key={index}>{user.username}</li>
+    )
+
     return (
       <div>
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
+          {users}
+
           {/* In addition, display the total number of users curently in the store */}
+          User Count: {this.props.userCount}
+
         </ul>
       </div>
     )
@@ -16,6 +26,12 @@ class Users extends Component {
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+  return { 
+    users: state.users, 
+    userCount: state.users.length
+   }
+}
 
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { createStore } from "redux";  // add imports
+import { Provider } from "react-redux"; // add imports
 import manageUsers from './reducers/manageUsers'
 
+const store = createStore(    //add code
+  manageUsers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// add imports and code
-
-
-ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
+ReactDOM.render(          //add code
+  <Provider store={store}>      
+      <App />
+  </Provider> ,
   document.getElementById('root')
 );
